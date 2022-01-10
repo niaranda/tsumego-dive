aadb2903e2562cf2136c5cfa6500249d73ae9df6
Lala <fontnina@gmail.com>
2022-01-10 22:57:58 +0100

    Pre - Added error handling in problem_processing

--- src/preprocessing/problem_processing.py
+++ src/preprocessing/problem_processing.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 import sgf
+
+from src.preprocessing.error_handling import log_error
 
 
 def process_problem(problem_name: str):
@@ -6,7 +10,14 @@ def process_problem(problem_name: str):
     problem = __parse_problem(problem_name)
 
 
-def __parse_problem(problem_path: str) -> sgf.GameTree:
+def __parse_problem(problem_path: str) -> Optional[sgf.GameTree]:
     """Parses problem in given path"""
     with open(problem_path, encoding="GB2312", errors="ignore") as file:
-        return sgf.parse(file.read()).children[0]  # only one problem per file
+        try:
+            return sgf.parse(file.read()).children[0]  # only one problem per file
+        except UnicodeDecodeError as e:
+            log_error(e, problem_path)
+            return None
+        except sgf.ParseException as e:
+            log_error(e, problem_path)
+            return None
