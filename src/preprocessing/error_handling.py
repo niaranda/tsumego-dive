b8602710dac5bf0a45045bf0f0e5a049a7094f75
Lala <fontnina@gmail.com>
2022-01-10 22:57:20 +0100

    Pre - Added error_handling module

---
+++ src/preprocessing/error_handling.py
@@ -0,0 +1,12 @@
+import logging
+import os
+from datetime import datetime
+
+now = datetime.now().strftime("%y%m%d_%H%M%S")
+os.mkdir(f"../../log/{now}")
+logging.basicConfig(filename=f"../../log/{now}/errors.log")
+
+
+def log_error(e: Exception, problem_path: str):
+    """Logs an exception raised while processing a problem in given path"""
+    logging.error(f"{type(e)} in {problem_path}")
