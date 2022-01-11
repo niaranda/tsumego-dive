bf6825d33b6a669aaa9dbdc6ebb02765218c8636
Lala <fontnina@gmail.com>
2022-01-11 20:10:20 +0100

    Pre - Added sgf parsing in problem_processing

--- src/preprocessing/problem_processing.py
+++ src/preprocessing/problem_processing.py
@@ -1,4 +1,12 @@
+import sgf
 
-def process_problem(problem: str):
+
+def process_problem(problem_name: str):
     """Processes one tsumego problem"""
-    print(problem)
+    problem = __parse_problem(problem_name)
+
+
+def __parse_problem(problem_path: str) -> sgf.GameTree:
+    """Parses problem in given path"""
+    with open(problem_path, encoding="GB2312", errors="ignore") as file:
+        return sgf.parse(file.read()).children[0]  # only one problem per file
