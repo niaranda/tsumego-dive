5fb5461efb70f1d86db26473a2134c50833e5e1d
Lala <fontnina@gmail.com>
2022-01-16 13:54:33 +0100

    Pre - Added Board#get_stones

--- src/preprocessing/beans/board.py
+++ src/preprocessing/beans/board.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 import numpy as np
 
 from src.preprocessing.beans.board_point import BoardPoint
-from src.preprocessing.beans.stone import Color
+from src.preprocessing.beans.stone import Color, Stone
 
 
 class Board:
@@ -13,6 +13,14 @@ class Board:
         self.__grid = np.empty((19, 19), dtype=BoardPoint)
         for index, _ in np.ndenumerate(self.__grid):
             self.__grid[index] = BoardPoint(index)
+
+    def get_stones(self) -> List[Stone]:
+        stones: List[Stone] = []
+        board_point: BoardPoint
+        for _, board_point in np.ndenumerate(self.__grid):
+            if not board_point.is_empty():
+                stones.append(board_point.stone)
+        return stones
 
     def place_stones(self, positions: List[Tuple[int, int]], color: Color):
         """Places a stone in the board"""
