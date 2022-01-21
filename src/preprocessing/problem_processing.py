5cce372e5de40de97db79ef8a9952af517b063d2
Lala <fontnina@gmail.com>
2022-01-21 21:33:50 +0100

    Pre - Added tree parsing to problem_processing

--- src/preprocessing/problem_processing.py
+++ src/preprocessing/problem_processing.py
@@ -2,14 +2,20 @@ from typing import Optional
 
 import sgf
 
+from src.beans.game_tree.game_tree import GameTree
+from src.preprocessing.sgf_tree_parsing import parse_sgf_tree
 from src.preprocessing.error_handling import log_error
 
 
 def process_problem(problem_name: str):
     """Processes one tsumego problem"""
-    problem = __parse_problem(problem_name)
+    problem: Optional[sgf.GameTree] = __parse_problem(problem_name)
+    if problem is None:
+        return
 
+    game_tree: GameTree = parse_sgf_tree(problem)
 
+    # TODO
 
 
 def __parse_problem(problem_path: str) -> Optional[sgf.GameTree]:
