4b0ece578ce9ba71041264b5ad59772888619492
Lala <fontnina@gmail.com>
2022-01-24 18:36:35 +0100

    Pre - Added GameTree#get_leaves and #get_nodes

--- src/beans/game_tree/game_tree.py
+++ src/beans/game_tree/game_tree.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from src.beans.game_tree.game_node import GameNode
 
 
@@ -8,3 +10,17 @@ class GameTree:
     @property
     def root(self):
         return self.__root
+
+    def get_leaves(self) -> List[GameNode]:
+        return list(filter(GameNode.is_leaf, self.get_nodes()))
+
+    def get_nodes(self, node: Optional[GameNode] = None) -> List[GameNode]:
+        if node is None:
+            node = self.__root
+        if node.is_leaf():
+            return [node]
+
+        nodes = [node]
+        for child in node.children:
+            nodes += self.get_nodes(child)
+        return nodes
