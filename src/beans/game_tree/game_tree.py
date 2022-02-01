c1f521121e0582f1eda2ada14d66d75019dbfcb7
Nina Aranda <fontnina@gmail.com>
2022-02-01 17:17:23 +0100

    Pre - Added comments to GameTree

--- src/beans/game_tree/game_tree.py
+++ src/beans/game_tree/game_tree.py
@@ -5,8 +5,13 @@ from src.preprocessing.path_type_analysis import compute_path_types
 
 
 class GameTree:
+    """Represents a game tree for a tsumego"""
+
     def __init__(self, root: GameNode):
+        """Creates a new game tree with given node as root"""
         self.__root: GameNode = root
+
+        # perform path type analysis
         compute_path_types(self)
 
     @property
@@ -14,9 +19,11 @@ class GameTree:
         return self.__root
 
     def get_leaves(self) -> List[GameNode]:
+        """Get list of all leaves"""
         return list(filter(GameNode.is_leaf, self.get_nodes()))
 
     def get_nodes(self, node: Optional[GameNode] = None) -> List[GameNode]:
+        """Recursively get list of all nodes in the tree"""
         if node is None:
             node = self.__root
         if node.is_leaf():
