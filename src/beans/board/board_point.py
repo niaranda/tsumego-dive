56caec9336cedde1211551ccc49d9a12eb379620
Nina Aranda <fontnina@gmail.com>
2022-02-01 16:34:23 +0100

    Pre - Added comments to BoardPoint

--- src/beans/board/board_point.py
+++ src/beans/board/board_point.py
@@ -10,6 +10,7 @@ class BoardPoint:
     """Represents a board point"""
 
     def __init__(self, pos: Pos):
+        """Creates an empty board point in given board position"""
         self.__stone: Optional[Stone] = None
         self.__pos: Pos = pos
 
@@ -33,7 +34,9 @@ class BoardPoint:
         return self.__pos
 
     def is_empty(self) -> bool:
+        """True if the point is empty"""
         return self.__stone is None
 
     def remove_stone(self):
+        """Removes the placed stone"""
         self.__stone = None
