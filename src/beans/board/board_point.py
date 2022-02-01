25112f06097d6a5b7acc61315717b5259d813f4b
Nina Aranda <fontnina@gmail.com>
2022-02-01 16:21:17 +0100

    Pre - Removed unused BoardPoint#place_stone

--- src/beans/board/board_point.py
+++ src/beans/board/board_point.py
@@ -32,12 +32,6 @@ class BoardPoint:
     def pos(self) -> Pos:
         return self.__pos
 
-    def place_stone(self, color: Color):
-        """Places a stone of given color in this board point"""
-        if self.__stone is not None:
-            raise Exception("Stone placement error")
-        self.__stone = Stone(color, self.pos)
-
     def is_empty(self) -> bool:
         return self.__stone is None
 
