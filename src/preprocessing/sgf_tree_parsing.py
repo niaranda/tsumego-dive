a087d8029294dfce6e1bb966942094393febb693
Nina Aranda <fontnina@gmail.com>
2022-02-01 17:31:33 +0100

    Pre - Added comments to sgf_tree_parsing module

--- src/preprocessing/sgf_tree_parsing.py
+++ src/preprocessing/sgf_tree_parsing.py
@@ -12,11 +12,14 @@ Pos = Tuple[int, int]
 
 
 def parse_sgf_tree(problem: sgf.GameTree) -> GameTree:
+    """Converts an sgf game tree to a game tree for this project"""
+    # Initialize first board state
     init_stones: List[Stone] = __get_init_stones(problem)
 
     init_board: Board = Board(init_stones)
     root = GameNode(None, init_board, None)
 
+    # recursively add all branches
     first_color: Color = __get_first_color(problem)
     __add_branches(problem.children, root, first_color)
 
@@ -24,6 +27,7 @@ def parse_sgf_tree(problem: sgf.GameTree) -> GameTree:
 
 
 def __get_init_stones(problem: sgf.GameTree) -> List[Stone]:
+    """Gets sgf game tree initial stones"""
     properties: dict = problem.root.properties
 
     black_stones: List[Stone] = __parse_init_stones(properties, Color.BLACK)
@@ -46,8 +50,9 @@ def __parse_stone(properties: dict, color: Color) -> Stone:
     property_name: str = "B" if color == Color.BLACK else "W"
 
     if property_name not in properties.keys():
-        raise Exception()
+        raise Exception(f"Not found expected {color} stone property while parsing sgf tree")
 
+    # Get position from properties
     pos: Pos = __parse_position(properties.get(property_name)[0])
     return Stone(color, pos)
 
@@ -60,13 +65,14 @@ def __parse_position(str_position: str) -> Pos:
 def __get_first_color(problem: sgf.GameTree) -> Color:
     keys: List[str] = list(problem.children[0].root.properties.keys())
     if "B" in keys and "W" in keys:
-        raise Exception()
+        raise Exception("Found stones for both colors while parsing sgf tree root")
     if "B" not in keys and "W" not in keys:
-        raise Exception()
+        raise Exception("Found no stone while parsing sgf tree root")
     return Color.BLACK if "B" in keys else Color.WHITE
 
 
 def __add_branches(branches: List[sgf.GameTree], game_node: GameNode, color: Color):
+    """Recursively adds all branches"""
     for branch in branches:
         new_game_node = game_node
         next_color = color
@@ -78,11 +84,13 @@ def __add_branches(branches: List[sgf.GameTree], game_node: GameNode, color: Col
 
 
 def __create_game_node(properties: dict, game_node: GameNode, color: Color) -> GameNode:
+    """Creates a new game node with given parent node by adding a new stone of given color to the board"""
     new_stone: Stone = __parse_stone(properties, color)
-    new_board: Board = copy.deepcopy(game_node.board)
+    new_board: Board = copy.deepcopy(game_node.board)  # Copy current board
 
-    comment: str = properties.get("C")
+    comment: str = properties.get("C")  # Can be None
 
+    # Place the new stone and create a new game node
     new_board.place_stones([new_stone])
     new_game_node = GameNode(game_node, new_board, new_stone, comment)
 
