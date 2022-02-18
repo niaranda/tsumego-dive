6fabb261271d77c88f90355ff3f033f61b311d2d
Nina Aranda <fontnina@gmail.com>
2022-02-18 11:16:17 +0100

    Pre - Added try-catch to problem_processing to deal with exceptions

--- src/preprocessing/problem_processing.py
+++ src/preprocessing/problem_processing.py
@@ -9,13 +9,18 @@ from src.preprocessing.error_handling import log_error
 
 def process_problem(problem_name: str):
     """Processes one tsumego problem"""
+    print(problem_name)
     problem: Optional[sgf.GameTree] = __parse_problem(problem_name)
     if problem is None:
         return
 
-    game_tree: GameTree = parse_sgf_tree(problem)
+    try:
+        game_tree: GameTree = parse_sgf_tree(problem)
 
-    # TODO
+        # TODO
+    except Exception as e:
+        log_error(e, problem_name)
+
 
 
 def __parse_problem(problem_path: str) -> Optional[sgf.GameTree]:
