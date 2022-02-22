0f88184650ba1ef28fce980884792497a6b81f7c
Nina Aranda <fontnina@gmail.com>
2022-02-22 22:18:11 +0100

    Pre - Added ko rule test

--- test/beans/game_tree/test_game_node.py
+++ test/beans/game_tree/test_game_node.py
@@ -1,7 +1,9 @@
 import unittest
+from copy import copy
 from typing import Optional
 
 from src.beans.board.board import Board
+from src.beans.board.color import Color
 from src.beans.game_tree.game_node import GameNode, PathType
 
 
@@ -9,7 +11,6 @@ def _create_game_node(parent: Optional[GameNode], path_type: PathType = PathType
     node = GameNode(parent, Board(), None, None)
     node.path_type = path_type
     return node
-
 
 class TestGameNode(unittest.TestCase):
 
@@ -63,3 +64,21 @@ class TestGameNode(unittest.TestCase):
         self.assertFalse(node1.is_correct())
         self.assertTrue(node2.is_correct())
         self.assertFalse(node3.is_correct())
+
+    def test_ko_rule(self):
+        black_stones = [((0, 1), Color.BLACK), ((1, 0), Color.BLACK)]
+        white_stones = [((0, 2), Color.WHITE), ((1, 1), Color.WHITE)]
+        board1 = Board(black_stones + white_stones)
+        root = GameNode(None, board1, None)
+
+        board2 = copy(board1)
+        stone = ((0, 0), Color.WHITE)
+        board2.place_stone(stone)
+        node = GameNode(root, board2, stone)
+
+        board3 = copy(board2)
+        illegal_stone = ((0, 1), Color.BLACK)
+        board3.place_stone(illegal_stone)
+
+        with self.assertRaises(Exception):
+            GameNode(node, board3, illegal_stone)
