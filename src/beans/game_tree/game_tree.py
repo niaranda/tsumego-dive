0d2737b1e3bb3f59afd917bab2d1a3cce641b32f
Nina Aranda <fontnina@gmail.com>
2022-02-24 20:46:58 +0100

    Pre - Moved path_type_analysis module to preprocessing directory

--- src/beans/game_tree/game_tree.py
+++ src/beans/game_tree/game_tree.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
 from src.beans.game_tree.game_node import GameNode
-from src.beans.game_tree.path_type_analysis import PathTypeAnalyser
+from src.preprocessing.path_type_analysis import PathTypeAnalyser
 
 
 class GameTree(PathTypeAnalyser):
