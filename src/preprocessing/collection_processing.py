5c072e4eb5ccdf6bca40d9968143a2178ca63628
Nina Aranda <fontnina@gmail.com>
2022-02-26 21:34:34 +0100

    Pre - Added methods to deal with input data generation and saving in collection_processing

--- src/preprocessing/collection_processing.py
+++ src/preprocessing/collection_processing.py
@@ -1,5 +1,9 @@
 import os
-from typing import List
+from typing import List, Tuple
+
+import dotenv
+import numpy as np
+import pandas as pd
 
 from problem_processing import process_problem
 
@@ -7,9 +11,40 @@ from problem_processing import process_problem
 def process_collection(collection_path: str):
     """Processes a tsumego collection in given path"""
     problem_paths = __get_problem_paths(collection_path)
+    black_moves, white_moves = __process_problems(problem_paths)
+
+    __save_to_csv(black_moves, white_moves)
+
+
+def __save_to_csv(black_moves: np.ndarray, white_moves: np.ndarray):
+    dotenv.load_dotenv()
+
+    with open(os.environ["BLACK_FILE"], "a") as f:
+        f.write(pd.DataFrame(black_moves).to_csv(header=False))
+
+    with open(os.environ["WHITE_FILE"], "a") as f:
+        f.write(pd.DataFrame(white_moves).to_csv(header=False))
+
+
+def __process_problems(problem_paths: List[str]) -> Tuple[np.ndarray, np.ndarray]:
+    black_moves, white_moves = None, None
+
     for problem_path in problem_paths:
-        if __is_valid(problem_path):
-            process_problem(problem_path)
+        if not __is_valid(problem_path):
+            continue
+
+        problem_result = process_problem(problem_path)
+        if problem_result is None:
+            continue
+
+        if black_moves is None:
+            black_moves, white_moves = problem_result
+            continue
+
+        black_moves = np.vstack([black_moves, problem_result[0]])
+        white_moves = np.vstack([white_moves, problem_result[1]])
+
+    return black_moves, white_moves
 
 
 def __get_problem_paths(collection_path: str) -> List[str]:
