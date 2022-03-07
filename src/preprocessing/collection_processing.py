7c2dae6b2a11f6756fcd30bc078a2184cddbccb3
Nina Aranda <fontnina@gmail.com>
2022-03-07 19:04:24 +0100

    Pre - Added comments and fixed type hint in collection_processing

--- src/preprocessing/collection_processing.py
+++ src/preprocessing/collection_processing.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import dotenv
 import numpy as np
@@ -17,6 +17,7 @@ def process_collection(collection_path: str):
 
 
 def __save_to_csv(black_moves: np.ndarray, white_moves: np.ndarray):
+    """Saves given data to csv files"""
     dotenv.load_dotenv(override=True)
 
     with open(os.environ["BLACK_FILE"], "a") as f:
@@ -27,27 +28,28 @@ def __save_to_csv(black_moves: np.ndarray, white_moves: np.ndarray):
 
 
 def __process_problems(problem_paths: List[str]) -> Tuple[np.ndarray, np.ndarray]:
+    """Returns data obtained by processing problems in given paths"""
     black_moves, white_moves = None, None
 
     for problem_path in problem_paths:
-        if not __is_valid(problem_path):
+        if not __is_valid(problem_path):  # Validate path
             continue
 
-        problem_result = process_problem(problem_path)
-        if problem_result is None:
+        problem_result: Optional[Tuple[np.ndarray, Optional[np.ndarray]]] = process_problem(problem_path)
+        if problem_result is None:  # the problem had an irresolvable error
             continue
 
-        if black_moves is None:
+        if black_moves is None:  # first problem
             black_moves, white_moves = problem_result
             continue
 
         black_moves = np.vstack([black_moves, problem_result[0]])
 
-        if white_moves is None:
+        if white_moves is None:  # white moves can be None if there were no white moves in previous problems
             white_moves = problem_result[1]
             continue
 
-        if problem_result[1] is not None:
+        if problem_result[1] is not None:  # if this problem has white moves
             white_moves = np.vstack([white_moves, problem_result[1]])
 
     return black_moves, white_moves
