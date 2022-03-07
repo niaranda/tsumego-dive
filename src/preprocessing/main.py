c707ac86fc1116d168b395e9f1fb9778b4f24e4c
Nina Aranda <fontnina@gmail.com>
2022-03-07 19:17:06 +0100

    Pre - Added comments to main

--- src/preprocessing/main.py
+++ src/preprocessing/main.py
@@ -9,23 +9,31 @@ from src.preprocessing.input_data_generation import truncate_csv_files
 
 
 def main():
+    # Get collection paths
     collection_paths = os.listdir("../../raw_data")
     if ".keep" in collection_paths:
         collection_paths.remove(".keep")
 
+    # Remove skipped collections
     collection_paths = list(filter(lambda collection: "skip" not in collection, collection_paths))
 
     for collection_number in range(60):
         collection_path = collection_paths[collection_number]
 
+        # Define necessary environment variables
         dotenv.load_dotenv()
 
         dotenv.set_key(dotenv.find_dotenv(), "BLACK_FILE", f"../../input_data/black_{collection_number}.csv")
         dotenv.set_key(dotenv.find_dotenv(), "WHITE_FILE", f"../../input_data/white_{collection_number}.csv")
         dotenv.set_key(dotenv.find_dotenv(), "DEFAULT_WRONG", str("wrong" in collection_path))
 
+        # Truncate existing csv files
         truncate_csv_files()
+
+        # Perform processing
         process_collection(collection_path)
+
+        # Force garbage collection
         gc.collect()
 
 
