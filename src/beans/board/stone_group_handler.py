ecca37dd662db1325a2489844bf001ab0ccb5c1c
Nina Aranda <fontnina@gmail.com>
2022-03-07 18:41:15 +0100

    Pre - Added comments to StoneGroupHandler

--- src/beans/board/stone_group_handler.py
+++ src/beans/board/stone_group_handler.py
@@ -9,9 +9,10 @@ from src.preprocessing.preprocessing_exception import PreprocessingException
 
 
 class StoneGroupHandler:
-    """Class to handle stone groups in board"""
+    """Handles stone groups in board"""
 
     def __init__(self, stones: Optional[List[Stone]]):
+        # Add all stones to groups
         self.__stone_groups: List[StoneGroup] = []
         if stones:
             for stone in stones:
@@ -30,7 +31,7 @@ class StoneGroupHandler:
         pos, color = stone
         neighbor_groups: List[StoneGroup] = self._get_neighbor_groups_of_color(pos, color)
 
-        # No neighbor group -> the stone is a new group
+        # No neighbor group -> the stone is itself a new group
         if len(neighbor_groups) == 0:
             self.__add_group(StoneGroup([pos], color))
             return
@@ -43,31 +44,32 @@ class StoneGroupHandler:
             self.__fuse_groups(neighbor_groups)
 
     def _get_neighbor_groups_of_color(self, position: Pos, color: Color) -> List[StoneGroup]:
-        """Get the given stone's neighbor groups of given color"""
+        """Returns the list of groups that are neighbors to given position and have given color"""
         groups: List[StoneGroup] = self.__get_groups_of_color(color)
         return list(filter(lambda group: group.is_attached(position), groups))
 
     def _remove_groups(self, groups: List[StoneGroup]):
-        """Remove a list of groups"""
+        """Removes a list of groups"""
         for group in groups:
             self.__stone_groups.remove(group)
 
     def _get_group_containing(self, position: Pos) -> StoneGroup:
+        """Returns group containing given position"""
         for group in self.__stone_groups:
             if position in group.positions:
                 return group
         raise PreprocessingException(f"Added stone in {position} is not contained in any group")
 
     def __get_groups_of_color(self, color: Color) -> List[StoneGroup]:
-        """Get all groups of a given color"""
+        """Returns all groups of a given color"""
         return list(filter(lambda group: group.color == color, self.__stone_groups))
 
     def __add_group(self, group: StoneGroup):
-        """Add a new group"""
+        """Adds a new group to the list"""
         self.__stone_groups.append(group)
 
     def __fuse_groups(self, groups: List[StoneGroup]):
-        """Fuse given groups into one"""
+        """Fuses given groups into one"""
         # Remove groups
         self._remove_groups(groups)
 
